b7766a541f58917a466213bc8a243f851610ecf9
ty-peng <tiany.dev@qq.com>
2020-12-31 10:31:18 +0800

    feat: spring-boot-elasticsearch project

---
+++ spring-boot-elasticsearch/src/test/java/com/typeng/demo/elasticsearch/ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.typeng.demo.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
